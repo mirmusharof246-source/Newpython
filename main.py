3d5f0202ce9b4d71e22106cbd06d0c985e4fe4f0
Musharof <mirmusharof246@gmail.com>
2026-06-06 22:59:46 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+GK="string type data"
+print(GK)
+#int type data
+Hablu=420
+print(type(Hablu))
+
+#floating type data
+
+gablu=40.5
+print(type(gablu))
+
+#complex type data
+kodu=420j
+
+
+print(type(kodu))
+
+#string type data
+Myname="Musharof"
+kipi="karimgamj"
+print('My name is'+' '+Myname)
+
